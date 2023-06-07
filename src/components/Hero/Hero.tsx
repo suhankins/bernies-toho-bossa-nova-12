2a829f7240c07741020286d6fc41e2623432384d
Dimitri Sukhankin <punishedbernadetta@niuitmo.ru>
2023-06-07 17:52:30 +0400

    made hero component responsive

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,19 +1,28 @@
 import heroImage from './img/hero.webp';
+import heroImageMobile from './img/hero-mobile.webp';
 import './Hero.css';
 
 export function Hero() {
     return (
         <header className="hero">
-            <img
-                className="hero-image-blured"
-                src={heroImage}
-                aria-hidden="true" // This is just a decoration, so it's not important for screen readers
-            />
-            <img
-                className="hero-image"
-                src={heroImage}
-                alt="Cover of Toho Bossa Nova 12 album, showing a red haired anime girl wearing red coat gazing at viewer, with dusk in the background"
-            />
+            <picture>
+                <source srcSet={heroImageMobile} media="(max-width: 600px)" />
+                <source srcSet={heroImage} />
+                <img
+                    className="hero-image-blured"
+                    src={heroImage}
+                    aria-hidden="true" // This image is decorative, so it should be hidden from screen readers
+                />
+            </picture>
+            <picture>
+                <source srcSet={heroImageMobile} media="(max-width: 600px)" />
+                <source srcSet={heroImage} />
+                <img
+                    className="hero-image"
+                    src={heroImage}
+                    alt="Cover of Toho Bossa Nova 12 album, showing a red haired anime girl wearing red coat gazing at viewer, with dusk in the background"
+                />
+            </picture>
         </header>
     );
 }
