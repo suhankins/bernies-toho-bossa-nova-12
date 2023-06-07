749b842c82a0ea6e9b7f76e6352391f9aad1878f
Dimitri Sukhankin <punishedbernadetta@niuitmo.ru>
2023-06-07 18:05:35 +0400

    added album description

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,18 @@ export function App() {
     return (
         <div id="content">
             <Hero />
-            <SongList />
-            <VideoEmbed />
+            <main>
+                <section className="album-description">
+                    <h1>Toho Bossa Nova 12</h1>
+                    <h2>2023.05.07</h2>
+                    <p>イベント頒布価格1000円 第二十回博麗神社例大祭 た24ab</p>
+                    <p>ShibayanRecords にて頒布</p>
+                </section>
+                <section>
+                    <SongList />
+                </section>
+                <VideoEmbed />
+            </main>
             <Footer />
         </div>
     );
